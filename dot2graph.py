f1687e1cd565c8fa06a5943c44319cd3fd3fd1d9
yhryyq <yhryyq@163.com>
2023-12-23 18:04:56 -0800

    add tqdm

--- dot2graph.py
+++ dot2graph.py
@@ -4,6 +4,8 @@ from collections import namedtuple,deque
 from filter import checkProject
 import sys
 sys.setrecursionlimit(3000)
+from tqdm import tqdm
+
 """
 def dfs(graph, start_vertex, vertex_map):
     visited = set()
@@ -277,7 +279,7 @@ def edge_exists(graph, source_vertex, target_vertex):
 print("========getting the function info")
 NodeData = namedtuple('NodeData', ['line_number', 'line_flows', 'node_code', 'callsites', 'is_return_line', 'function_name', 'file_name'])
 
-funcs = read_dot_files("./outdir_scipy_8fa4b7364d98659dd8fe28727f60d99a14b95850_pyc")
+funcs = read_dot_files("./outdir_numpy_028bdf88e6c71a705d0c6bfd55d1117c7f44f6aa_pyc")
 #funcs = read_dot_files("./outdir_cpython_pyc")
 #funcs: filename, function_name, line_flows, callsites, node_code, return_lines
 
@@ -337,7 +339,7 @@ with open("vertex_maps.txt", "w") as file:
 
 print("========getting pyc mapping")
 #get the rule-based mapping
-cfunc, ctype = checkProject('./scipy')
+cfunc, ctype = checkProject('./numpy')
 seen = set()
 func_mapping = []
 for item in cfunc:
@@ -349,7 +351,7 @@ print(func_mapping)
 
 cross_lan_map=[]
 print("========linking the graphs")
-for func_name, func_data in graphs.items():
+for func_name, func_data in tqdm(graphs.items()):
     local_graph = func_data['graph']
     local_vertex_map = func_data['vertex_map']
     node_data_map = func_data['node_data_map']
@@ -423,8 +425,8 @@ for func_name, data in graphs.items():
 
 """
 #scipy
-function_name = "_get_spline_boundary_mode"
-start_line_number = 245
+function_name = "PyArray_AssignFromCache_Recursive"
+start_line_number = 555
 
 #cpython
 #function_name = "frozenset_new"
@@ -454,6 +456,6 @@ if function_name in vertex_maps and start_line_number in vertex_maps[function_na
     start_vertex = vertex_maps[function_name][start_line_number]
     #dfs(global_graph, start_vertex, global_node_data=global_node_data)
     #dfs_path(global_graph, start_vertex,global_node_data)
-    dfs_all_paths_backf(global_graph, start_vertex, set(), [], global_node_data,True)
+    dfs_all_paths(global_graph, start_vertex, set(), [], global_node_data,True)
 else:
     print(f"函数 {function_name} 或行号 {start_line_number} 不存在于图中。")
--- downloadsha.sh
+++ downloadsha.sh
@@ -1,7 +1,7 @@
 #shas=("a13a7b82d4664c767575097c4a60c0b5fcd1098a" "9fede9489d98707240d0e7213104bc617daa4c75" "c320f7e296651cb624857ec6d88df8fc10fd0bb1" "60dc9730d5652f0632cd43caef437f01a734e374" "8fa4b7364d98659dd8fe28727f60d99a14b95850")
 
-sha="8fa4b7364d98659dd8fe28727f60d99a14b95850"
-project="scipy/scipy"
+sha="028bdf88e6c71a705d0c6bfd55d1117c7f44f6aa"
+project="numpy/numpy"
 
 repo=$(echo $project | cut -d'/' -f2)
 git clone https://github.com/${project}.git
