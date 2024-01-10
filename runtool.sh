17424b4202776b962fb5824a3cf44cf74df8240e
yhryyq <yhryyq@163.com>
2024-01-09 19:54:45 -0800

    runtool process on 84

--- runtool.sh
+++ runtool.sh
@@ -1,8 +1,8 @@
 #shas=("a13a7b82d4664c767575097c4a60c0b5fcd1098a" "9fede9489d98707240d0e7213104bc617daa4c75" "c320f7e296651cb624857ec6d88df8fc10fd0bb1" "60dc9730d5652f0632cd43caef437f01a734e374" "8fa4b7364d98659dd8fe28727f60d99a14b95850")
 #projects=("scipy/scipy" "scipy/scipy" "scipy/scipy" "scipy/scipy" "scipy/scipy")
 
-start_line=1
-end_line=10
+start_line=11
+end_line=20
 
 file_name="casestudy_2.csv"
 
