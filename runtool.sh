fefd18f1489521874302ab04a52cf0dc178ad451
yhryyq <yhryyq@163.com>
2024-02-08 18:29:42 -0800

    add two new csv

--- runtool.sh
+++ runtool.sh
@@ -2,8 +2,8 @@
 #projects=("scipy/scipy" "scipy/scipy" "scipy/scipy" "scipy/scipy" "scipy/scipy")
 
 
-start_line=1
-end_line=20
+start_line=76
+end_line=90
 
 file_name="pyc_inter_bug_commits_151+.csv" #casestudy.csv casestudy_2.csv pyc_inter_bug_commits_wen.csv pyc_inter_bug_commits_temp.csv
 
